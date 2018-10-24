39a7db2232866d97b85f99d6ca95fc44e6a7c371
ikumu <196thinline@gmail.com>
2018-10-24 11:44:14 +0900

    test application.html.erb

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,34 +1,28 @@
 require 'rails_helper'
 
 describe StaticPagesController, type: :controller do
+  render_views
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+  
   describe 'GET #home' do
     it 'has a 200 status code' do
       get 'home'
       expect(response).to have_http_status(:ok)
+      assert_select "title", "Home | #{base_title}"
     end
-    #it 'find a title tag' do
-    #  visit '/static_pages/home'
-    #  expect(page).to have_title 'Home | Ruby on Rails Tutorial Sample App'
-    #end
   end
   describe 'GET #help' do
     it 'has a 200 status code' do
       get 'help'
       expect(response.status).to eq 200
+      assert_select "title", "Help | #{base_title}"
     end
-    #it 'find a title tag' do
-    #  visit 'help'
-    #  expect(page).to have_selector 'title',text: 'Home | Ruby on Rails Tutorial Sample App'
-    #end 
   end
   describe 'GET #about' do
     it 'has a 200 status code' do
       get 'about'
       expect(response.status).to eq 200
+      assert_select "title", "About | #{base_title}"
     end
-    #it 'find a title tag' do
-    #  visit 'about'
-    #  expect(page).to have_selector 'title',text: 'Home | Ruby on Rails Tutorial Sample App'
-    #end
   end
 end
