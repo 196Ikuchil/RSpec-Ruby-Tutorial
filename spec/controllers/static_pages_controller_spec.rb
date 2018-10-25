8e32997cb6aea04d5173b60f781ed58c7ef8b792
ikumu <196thinline@gmail.com>
2018-10-25 18:45:51 +0900

    ルーティングの変更&それに伴うテストの修正

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'spec_helper'
 
 describe StaticPagesController, type: :controller do
   render_views
--- spec/features/application_title_spec.rb
+++ spec/features/application_title_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 feature 'Application', type: :feature do
   let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
   scenario 'get home title' do
-    visit 'static_pages/home'
+    visit '/'
     expect(page).to have_title("#{base_title}",exact: true )
   end
   scenario 'get help title' do
-    visit 'static_pages/help'
+    visit '/help'
     expect(page).to have_title("Help | #{base_title}",exact:true)
   end
   scenario 'get about title' do
-    visit 'static_pages/about'
+    visit '/about'
     expect(page).to have_title("About | #{base_title}",exact:true)
   end
   scenario 'get contact title' do
-    visit 'static_pages/contact'
+    visit '/contact'
     expect(page).to have_title("Contact | #{base_title}", exact:true)
   end
 end
