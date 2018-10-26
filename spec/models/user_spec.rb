6a706da67a6a7234bc48909d69d52f6560fb3351
ikumu <196thinline@gmail.com>
2018-10-26 17:06:35 +0900

    userモデルの属性に対するバリデーションの設定&テスト

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  before :example do
+    @user = create(:user)
+  end
+  it 'name should be valid' do
+    expect(@user).to be_valid
+  end
+
+  it 'name should be present' do
+    @user.name="   "
+    expect(@user).not_to be_valid
+  end
+
+  it 'email should be present' do
+    @user.email= "  "
+    expect(@user).not_to be_valid
+  end
+  
+  it 'name should not be too long' do
+    @user.name = 'a'*51
+    expect(@user).not_to be_valid
+  end
+
+  it 'email should not be too long' do
+    @user.email = 'a'*244+'@example.com'
+    expect(@user).not_to be_valid
+  end
+end
