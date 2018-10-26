60a975a2e3c2faaad3ec4ab4cf21683a73708430
ikumu <196thinline@gmail.com>
2018-10-26 17:38:15 +0900

    user.emailのバリデーションの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,4 +28,26 @@ RSpec.describe User, type: :model do
     @user.email = 'a'*244+'@example.com'
     expect(@user).not_to be_valid
   end
+
+  it 'email validation should accept valid addresses' do
+    valid_addresses = %w[user@example,com user_at_foo.org user.name@example.foo@bar_baz.com foo@bar+baz.com]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      expect(@user).not_to be_valid,  "#{valid_address.inspect} should be invalid"
+    end
+  end
+
+  it 'email addresses should be unique' do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    expect(duplicate_user).not_to be_valid
+  end
+
+  it 'email addresses should be saved as lower-case' do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    expect(@user.reload.email).to eq mixed_case_email.downcase
+  end
 end
