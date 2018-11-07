a8e670ffd8a98fb61cbf47eedaf5f2ac58167d1e
ikumu <196thinline@gmail.com>
2018-11-07 17:58:22 +0900

    signup後の処理とページ遷移の実装

--- spec/features/users_signip_spec.rb
+++ spec/features/users_signip_spec.rb
@@ -2,6 +2,52 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe 'Users Signup page', type: :request do
+  describe 'success signup' do
+    it 'ユーザの登録に成功する' do
+      get '/signup'
+      expect do
+        user = build(:user)
+        post '/users', params: {
+          user: {
+            name:   user.name,
+            email:  user.email,
+            password: user.password,
+            password: user.password_confirmation 
+        }
+      }
+      end.to change{User.count}
+    end
+
+    it 'リダイレクトする' do
+      get '/signup'
+      user = build(:user)
+      post '/users', params: {
+        user: {
+          name:   user.name,
+          email:  user.email,
+          password: user.password,
+          password: user.password_confirmation 
+        }
+      }
+      created = User.find_by(email: user.email)
+      expect(response).to redirect_to("/users/#{created.id}")
+    end
+
+    it 'flushに値が入っている' do
+      get '/signup'
+      user = build(:user)
+      post '/users', params: {
+        user: {
+          name:   user.name,
+          email:  user.email,
+          password: user.password,
+          password: user.password_confirmation 
+        }
+      }
+      expect(flash[:success]).not_to be_empty
+    end
+  end
+
   describe 'invalid signup information' do
     it 'ユーザ数が増えない' do
       get '/signup'
