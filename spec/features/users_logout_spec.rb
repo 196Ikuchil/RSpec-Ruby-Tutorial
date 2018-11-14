46c505a79d09fc497a4032780512972c4cdf61d7
ikumu <196thinline@gmail.com>
2018-11-14 12:56:57 +0900

    ２ブラウザからのログアウト時にエラーが出ないようを修正

--- spec/features/users_logout_spec.rb
+++ spec/features/users_logout_spec.rb
@@ -12,4 +12,16 @@ feature "UserLogout",type: :feature do
     expect(page).to have_current_path(root_path)
   end
 
+  scenario 'ログアウト後別ブラウザからログアウトを実行' do
+    visit '/login'
+    fill_in_login_form(user)
+    click_button "Log in"
+    expect(page).to have_current_path(user_path(user))
+    click_link "Log out"
+    expect(page).to have_current_path(root_path)
+    page.driver.submit :delete, "/logout", {}
+    expect(page).to_not have_current_path(logout_path)
+    expect(page).to_not have_current_path(user_path(user))
+  end
+
 end
